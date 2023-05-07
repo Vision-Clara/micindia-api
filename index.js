cc716d4f6a6405d247c50d7049469ef6a200ed61
sub1120 <subhamsahu1120@gmail.com>
2023-05-07 12:18:10 +0530

    setup moongoose and db

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const app = require("./app");
+const connectDB = require("./config/db");
 const { PORT } = process.env || 4000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
